ded7457038e42715f1cf57b1ef25f0d8ff9d1d3c
Lauri.L <late4460@gmail.com>
2023-06-08 18:40:49 +0300

    5.b

---
+++ part5/bloglist-frontend/src/components/BlogSubmitForm.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable linebreak-style */
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+
+const BlogSubmitForm = ({ submitBlog }) => {
+
+  BlogSubmitForm.propTypes = {
+    submitBlog:PropTypes.func.isRequired
+  }
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+
+  const addBlog=(event) => {
+    event.preventDefault()
+    submitBlog({ title:title, author:author, url:url })
+    setAuthor('')
+    setTitle('')
+    setUrl('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={addBlog}>
+        <h2>create new</h2>
+        <div className="submitUser">title: <input value={title} onChange={event => setTitle(event.target.value)} />
+        author: <input value={author} onChange={event => setAuthor(event.target.value)} />
+        url: <input value={url} onChange={event => setUrl(event.target.value)} />
+          <button type="submit">create</button></div>
+      </form>
+    </div>
+  )
+}
+
+export default BlogSubmitForm
